00d2041d15f0e50241e5ddd9ab754d54985075d1
VisAwesme <104176687+VisAwesme@users.noreply.github.com>
2025-05-12 09:39:35 -0400

    bro I fucking gave up and just used copilot to fix it, if it don't work blame copilot

--- js/disclaimer.js
+++ js/disclaimer.js
@@ -1,12 +1,10 @@
 // disclaimer.js
 
-// check if the user has already visited and acknowledged the disclaimer
 if (localStorage.getItem('hasVisited') === 'true') {
     window.location.href = "../index.html";
 }
 
 document.getElementById("okay-btn").onclick = function() {
-    // store in localStorage so they won't see this again
     localStorage.setItem('hasVisited', 'true');
     
     window.location.href = "../index.html";
